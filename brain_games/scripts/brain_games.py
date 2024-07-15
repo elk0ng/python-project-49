6143a51689f2dab3055110c8269d5a66ff7e99bf
elk0ng <elk0ng@icloud.com>
2024-07-15 16:11:07 +0300

    Changes to be committed: 	new file:   brain_games/__pycache__/cli.cpython-39.pyc 	new file:   brain_games/cli.py 	modified:   brain_games/scripts/__pycache__/brain_games.cpython-39.pyc 	modified:   brain_games/scripts/brain_games.py 	modified:   dist/hexlet_code-0.1.0-py3-none-any.whl 	modified:   dist/hexlet_code-0.1.0.tar.gz 	modified:   poetry.lock 	modified:   pyproject.toml

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+from brain_games.cli import welcome_user
+
 def main():
 	print ("Welcome to Brain Games!")
+	user_name = welcome_user()
+if __name__ == '__main__':
+    main()
