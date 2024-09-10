1870a94ed9c27b4dcf6f4870f9b0eacc9f88acb1
elk0ng <elk0ng92@gmail.com>
2024-09-10 10:01:23 +0300

    Refactoring

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,8 +3,7 @@ import random
 
 PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. ' \
                     'Otherwise answer "no".'
-MIN_NUMBER = random.randint(1, 20)
-MAX_NUMBER = random.randint(1, 20)
+
 
 def is_prime(number):
     divider = 2
@@ -14,7 +13,7 @@ def is_prime(number):
 
 
 def get_num_and_prime_ans():
-    num = get_random_num(1, 20)
+    num = random.randint(1, 20)
     answer = 'yes' if is_prime(num) else 'no'
     return str(num), answer
 
