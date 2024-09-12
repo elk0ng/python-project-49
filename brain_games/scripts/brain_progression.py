6527c058f97c666142f2f32a19058a237c877779
elk0ng <elk0ng92@gmail.com>
2024-09-12 16:48:36 +0300

    Refactoring

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from brain_games.engine import start_game
-from brain_games.games.progression import PROGRESSION_INSTRUCTION, get_progression_and_hidden_num
-
+from brain_games.games.progression import PROGRESSION_INSTRUCTION
+from brain_games.games.progression import get_progression_and_hidden_num
 
 def main():
     start_game(PROGRESSION_INSTRUCTION, get_progression_and_hidden_num)
